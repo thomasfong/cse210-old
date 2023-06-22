612caf6f62ba25547fa4e200490178fbb36c2173
thomasfong <thomasfong1020@gmail.com>
2023-06-22 13:44:07 +0800

    Done prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,13 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Ask the user for their name.
-        Console.WriteLine("What is your first name?");
-        string frist = Console.ReadLine();
+        Console.Write("What is your first name? ");
+        string FristName = Console.ReadLine();
 
-        Console.WriteLine("What is your last name?");
-        string last = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string LastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last}, {frist} {last}.");
+        Console.WriteLine($"Your name is {LastName}, {FristName} {LastName}.");
+
+        
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,37 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("This is C#");
+
+        Console.WriteLine("What is your are favorite color?");
+        String color = Console.ReadLine();
+        Console.Write($"You favorite color is {color}");
+   
+
     }
 }
-
-//A code tempkate for the category of things known as Person. The 
-// responsibility of a Person is to hold and display personal information.
-public class Person
-{
-    // The C# convention is to start menber variables with an underscore_
-    public string _givenName = "";
-    public string _familyName = "";
-
-    // A special method, called a constructor that is invoked using the 
-    // new keyword followed by the class =name and parentheses.
-    public Person()
-    {     
-    }
-
-    // A method that displays the person's full name as used in eastern
-    // countries or <family name, given name>.
-    public void ShowEasternName()
-    {
-        Console.WriteLine($"{_familyName}, {_givenName}");
-    }
-
-    // A method that displays the person's full name as used in western
-    // countries or <given name family name>.
-    public void ShowWesternName()
-    {
-        Console.WriteLine($"{_givenName} {_familyName}");
-    }
-}
-
 
